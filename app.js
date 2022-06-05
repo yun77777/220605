1e2e15b023b26b99b61c2a8cfb60f2ad3bf0418b
yun77777 <victoriousyk@naver.com>
2022-06-05 13:35:55 +0900

    commit'

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+});
+
+app.listen(PORT, console.log(`${PORT} is running`)); 
